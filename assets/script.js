c946c6c96ee718f8ca57eedab295245347695584
kjenkins5678 <kajenkins9@gmail.com>
2020-01-21 18:22:02 -0500

    found an older version of electron & electron-html-to, redoing index.html and script.js to not use cdns

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,33 @@
 var username = "octocat"
 
-var settings = {
-    "url": "https://api.github.com/users/" + username,
-    "method": "GET",
-    "timeout": 0,
-  };
+// var settings = {
+//     "url": "https://api.github.com/users/" + username,
+//     "method": "GET",
+//     "timeout": 0,
+//   };
   
-  $.ajax(settings).done(function (response) {
+//   $.ajax(settings).done(function (response) {
 
-    var profileImage = $("<img row>");
-    profileImage.attr("src", response.avatar_url);
-    profileImage.attr("alt", "Users Github Avatar Photo");
-    $(".jumbotron > .container").append(profileImage);
+//     var profileImage = $("<img row>");
+//     profileImage.attr("src", response.avatar_url);
+//     profileImage.attr("alt", "Users Github Avatar Photo");
+//     $(".jumbotron > .container").append(profileImage);
 
-    var name = $("<h1 row>");
-    name.text(response.name);
+//     var name = $("<h1 row>");
+//     name.text(response.name);
 
-    var org = $("<h2 row>");
-    org.text("Currently @ " + response.company);
+//     var org = $("<h2 row>");
+//     org.text("Currently @ " + response.company);
 
-    $(".jumbotron > .container").append(profileImage, name, org);
+//     $(".jumbotron > .container").append(profileImage, name, org);
 
 
+//   });
+
+  fetch('https://api.github.com/users/' + username)
+  .then((response) => {
+    return response.json();
+  })
+  .then((myJson) => {
+    console.log(myJson);
   });
--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-const fs = require('fs');
+// var fs = require('fs'),
+//     convertFactory = require('electron-html-to');
+ 
+// var conversion = convertFactory({
+//   converterPath: convertFactory.converters.PDF
+// });
+ 
+// conversion({ html: '<h1>Hello World</h1>' }, function(err, result) {
+//   if (err) {
+//     return console.error(err);
+//   }
+ 
+//   console.log(result.numberOfPages);
+//   console.log(result.logs);
+//   result.stream.pipe(fs.createWriteStream('the_result.pdf'));
+//   conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+// });
 
-const convertFactory = require('electron-html-to');
+const fs = require('fs');
+// const convertFactory = require('electron-html-to');
 fs.readFile('index.html', 'utf8', (err, htmlString) => {
   // add local path in case your HTML has relative paths
   htmlString = htmlString.replace(/href="|src="/g, match => {
-      console.log(match + '~/Documents/bootcamp2019/homework/Developer-Profile-Generator/');
-    return match + '~/Documents/bootcamp2019/homework/Developer-Profile-Generator/';
+    return console.log(match + 'file://path/to/you/base/public/directory');
   });
-
-
-//   const conversion = convertFactory({
-//     converterPath: convertFactory.converters.PDF,
-//     allowLocalFilesAccess: true
-//   });
-//   conversion({ html: htmlString }, (err, result) => {
-//     if (err) return console.error(err);
-//     result.stream.pipe(fs.createWriteStream('~/Documents/bootcamp2019/homework/test.pdf'));
-//     conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
-//   });
+  // const conversion = convertFactory({
+  //   converterPath: convertFactory.converters.PDF,
+  //   allowLocalFilesAccess: true
+  // });
+  // conversion({ html: htmlString }, (err, result) => {
+  //   if (err) return console.error(err);
+  //   result.stream.pipe(fs.createWriteStream('result.pdf'));
+  //   conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+  // });
 });
